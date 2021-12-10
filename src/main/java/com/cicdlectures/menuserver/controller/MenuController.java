149da82e6adde1511671618f74ba421c9e284521
rmasri <rouaaelmasri@gmail.com>
2021-12-10 13:05:40 +0000

    pull request

--- src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
+++ src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
@@ -46,7 +46,7 @@ public class MenuController {
     return createMenuService.createMenu(menu);
   }
 
-  
+  //comment on this 
   @DeleteMapping(path = "/menus/{id}")
   public void deleteMenu(@PathVariable Long id) {
     menuRepository.deleteById(id);
